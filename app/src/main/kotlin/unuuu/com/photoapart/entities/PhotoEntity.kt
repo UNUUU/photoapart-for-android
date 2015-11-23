4338b4d699ff8c3e16595c3545913a908c8422b8
t-kashima <takumi.kashima@nanapi.co.jp>
2015-11-23 21:58:26 +0900

    ユーザーの名前をタッチしてリンクできるようにしました

--- app/src/main/kotlin/unuuu/com/photoapart/entities/PhotoEntity.kt
+++ app/src/main/kotlin/unuuu/com/photoapart/entities/PhotoEntity.kt
@@ -1,5 +1,7 @@
 package unuuu.com.photoapart.entities;
 
-public class PhotoEntity(imageUrl: String) {
+public class PhotoEntity(imageUrl: String, userName: String, userUrl: String) {
     public val imageUrl: String = imageUrl
+    public val userName: String = userName
+    public val userUrl: String = userUrl
 }
--- app/src/main/kotlin/unuuu/com/photoapart/views/activities/MainActivity.kt
+++ app/src/main/kotlin/unuuu/com/photoapart/views/activities/MainActivity.kt
@@ -1,11 +1,14 @@
 package unuuu.com.photoapart.views.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import butterknife.bindView
 import com.squareup.otto.Subscribe
@@ -14,17 +17,22 @@ import unuuu.com.photoapart.R
 import unuuu.com.photoapart.events.BusHolder
 import unuuu.com.photoapart.models.PhotoModel
 import unuuu.com.photoapart.utils.DateUtil
-import java.util.Date
+import java.util.*
 
 class MainActivity: AppCompatActivity() {
 
-    val photoModel : PhotoModel = PhotoModel()
-    val imageView : ImageView by bindView(R.id.activity_main_frame_001)
-    val textView : TextView by bindView(R.id.activity_main_frame_002)
+    val photoModel: PhotoModel = PhotoModel()
+    val imageView: ImageView by bindView(R.id.activity_main_frame_001)
+    val dateView: TextView by bindView(R.id.activity_main_frame_002)
+    val userView: TextView by bindView(R.id.activity_main_frame_004)
+    val userLayout: LinearLayout by bindView(R.id.activity_main_layout_002)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userLayout.visibility = View.GONE
+        userView.setOnClickListener { openUserLink() }
     }
 
     override fun onResume() {
@@ -41,15 +49,24 @@ class MainActivity: AppCompatActivity() {
     @SuppressWarnings("unused")
     @Subscribe
     public fun onLoadedPhoto(event: PhotoModel.PhotoLoadedEvent) {
-        when (event.eventType) {
-            PhotoModel.PhotoLoadedEvent.EventType.COMPLETE -> {
-                Picasso.with(applicationContext).load(photoModel.photoEntity?.imageUrl).into(imageView)
-                textView.text = DateUtil.stringDateBySimpleDateFormat("HH:mm", photoModel.photoDate as Date)
-            }
-            PhotoModel.PhotoLoadedEvent.EventType.NETWORK_ERROR ->
-                println("写真のロードに失敗しました")
+        if (event.eventType == PhotoModel.PhotoLoadedEvent.EventType.COMPLETE) {
+            Picasso.with(applicationContext).load(photoModel.photoEntity?.imageUrl).into(imageView)
+            dateView.text = DateUtil.stringDateBySimpleDateFormat("HH : mm", photoModel.photoDate as Date)
+            userView.text = photoModel.photoEntity?.userName
+            userLayout.visibility = View.VISIBLE
+        } else if (event.eventType == PhotoModel.PhotoLoadedEvent.EventType.NETWORK_ERROR) {
+            println("写真のロードに失敗しました")
         }
         photoModel.pollingFetch()
+    }
+
+    /**
+     * ユーザーのページを開く
+     */
+    private fun openUserLink() {
+        val uri = Uri.parse(photoModel.photoEntity!!.userUrl);
+        val intent = Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
